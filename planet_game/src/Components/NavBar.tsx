2ff277b8502c58a30125584c919623ea6b974781
PurpleAsterDuchess <prinaparekh9@gmail.com>
2024-01-29 12:14:01 +0000

    add a nav bar

---
+++ planet_game/src/Components/NavBar.tsx
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+function NavBar() {
+
+  return (
+    <>
+      <div className='Nav'>
+        <p className='text'>
+          Text
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default NavBar;
