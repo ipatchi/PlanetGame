09d1d71197754ef95231c0d70947f58575c06e92
Ipatchi <ipatchilives@gmail.com>
2024-01-31 14:50:06 +0000

    completely revamped search bar to be more in style and ease of use improved

--- planet_game/src/App.tsx
+++ planet_game/src/App.tsx
@@ -1,10 +1,7 @@
-
-import Buttons from './Components/Buttons'
 import './App.css';
 import CentredScreen from './Components/CentredScreen';
 import NavBar from './Components/NavBar';
 import QuestionText from './Components/QuestionText';
-import ReviewDrop from './Components/ReviewDrop';
 import Search from './Components/Search';
 
 
@@ -15,12 +12,8 @@ function App() {
         <NavBar title="Planet Game"></NavBar>
         <CentredScreen>
           <br></br>
-          <QuestionText>Review</QuestionText>
-          <ReviewDrop title = "Question 1" question='Why is mars round?' correct={true} ></ReviewDrop>
-          <ReviewDrop title = "Question 2" question='Why is saturn ringy?' correct={false} ></ReviewDrop>
-          <Buttons></Buttons>
-          <Search
-            options={['-- SELECT --', 'Earth', 'Mars']}></Search>
+          <QuestionText>Learn</QuestionText>
+          <Search placeholder_text="Search The Skies..."></Search>
         </CentredScreen>
       </div>
     </>
--- planet_game/src/Components/Search.tsx
+++ planet_game/src/Components/Search.tsx
@@ -1,35 +1,70 @@
-import React, {ChangeEvent} from "react";
+import { useState } from 'react';
 import './Search.css';
-// need to access the selected item and create alert
-// make the options thing curved as well
 
-interface Props {
-    onChange:(value:string) => void;
-    options:string[];
-    selectedOption?:string;
-}
+const Search = ({ placeholder_text }: { placeholder_text?: string }) => {
+  //Function which handles any changes to the input box - resulting in changing the filter of shown planets
+  const typingHandler = (value: string) => {
+    setFilteredList([
+      ...list.filter((planet) =>
+        planet.toLowerCase().includes(value.toLowerCase())
+      ),
+    ]);
+    setShowList(value.length > 0);
+  };
 
-const Search: React.FC<Props> = ({ 
-    onChange,
-    options=[],
-    selectedOption
-  }) => { 
-    const handleOnChange = (e:ChangeEvent<HTMLSelectElement>) => {
-        onChange(e.target.value);
-    };
+  //Function to send the submitted choice back to the caller - just an alert for now
+  const submitChoiceHandler = (value: string) => {
+    alert('Click from planet ' + value);
+    doHideList();
+  };
+
+  //Basic functions to show/hide the list that can be called from various times
+  const doShowList = () => setShowList(true);
+  const doHideList = () => setShowList(false);
+
+  //List of things and usestate for the filtered list, and whether the list should be displayed or not
+  const list = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune',
+  ];
+  const [showList, setShowList] = useState(false);
+  const [filteredList, setFilteredList] = useState(list);
+
+  //Return all of the list items as h2 elements with their respective elements from the list
+  const displayedList = filteredList.map((data) => {
+    return (
+      <div className="result">
+        <h2
+          style={{ display: showList ? 'block' : 'none' }}
+          onClick={(e) => submitChoiceHandler(e.currentTarget.innerText)}
+        >
+          {data}
+        </h2>{' '}
+      </div>
+    );
+  });
+
+  //The TSX which is returned as a Search Bar
   return (
-    <select
-    onChange={handleOnChange}
-    value={selectedOption}
-    className={'search'}
-    >
-    {options.map((option, i) => (
-        <option value={i}>
-            {option}
-        </option>
-    ))}
-    </select>
+    <>
+      <div className="search">
+        <div onFocus={doShowList}>
+          <input
+            type="text"
+            placeholder={placeholder_text || 'Search...'}
+            onChange={(e) => typingHandler(e.target.value)}
+          ></input>
+          {displayedList}
+        </div>
+      </div>
+    </>
   );
-}
+};
 
-export default Search
+export default Search;
