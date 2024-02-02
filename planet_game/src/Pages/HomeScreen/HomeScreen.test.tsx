ae7eb2e31096d27395151f80e1a179787f425bef
PurpleAsterDuchess <prinaparekh9@gmail.com>
2024-02-02 12:49:13 +0000

    fix the test

--- planet_game/src/Pages/HomeScreen/HomeScreen.test.tsx
+++ planet_game/src/Pages/HomeScreen/HomeScreen.test.tsx
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import HomeScreen from './HomeScreen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-test('renders the landing page', () => {
-  render(<HomeScreen />);
-  expect(screen.getByRole('NavBar')).toHaveTextContent(/Planet Game/);
-  expect(screen.getByRole('button', { name: 'Start' })).toBeDisabled();
+describe('Button tests', () => {
+  test('renders the landing page', () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<HomeScreen />} />
+        </Routes>
+      </BrowserRouter>
+    );
+    expect(
+      screen.getByRole('button', {
+        name: /start/i,
+      })
+    ).toHaveProperty('disabled', false);
+  });
 });
