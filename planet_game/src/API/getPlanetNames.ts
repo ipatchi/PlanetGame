cfe6179e5266cef708f6487e2b8dd926ed87b925
PurpleAsterDuchess <prinaparekh9@gmail.com>
2024-02-27 14:25:46 +0000

    add types to the api requests

--- planet_game/src/API/getPlanetNames.ts
+++ planet_game/src/API/getPlanetNames.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { ResponseTypes } from './ResponseType';
+
 
 const getNames = async () => {
-  const resp = await axios.get('http://localhost:8080/planet');
+  const resp:ResponseTypes = await axios.get('http://localhost:8080/planet');
   const a: string[] = resp.data;
   return a;
 };
