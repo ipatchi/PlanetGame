08d82331dcc5e2f0203560b3da10b1073ee36333
ipatchi <ipatchilives@gmail.com>
2024-02-27 16:28:04 +0000

    added comment to rng

--- planet_game/src/rng.ts
+++ planet_game/src/rng.ts
@@ -1,3 +1,4 @@
+//A small random function which takes a minimum and maximum and returns an integer value of equal weighting between
 const randomInRange = (min: number, max: number) => {
   return Math.floor(Math.random() * (max + 1 - min));
 };
