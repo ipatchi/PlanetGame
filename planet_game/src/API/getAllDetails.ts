03207292d36a3d73f639f7c9b05b8b8f372ba9d9
Josh C <josh.carter@raytheon.co.uk>
2024-02-27 15:29:58 +0000

    attributes added to question gen

--- planet_game/src/API/getAllDetails.ts
+++ planet_game/src/API/getAllDetails.ts
@@ -1,24 +1,28 @@
 import axios from 'axios';
 
 interface SearchType {
-  data:{key:string}[]
+  data: { key: string }[];
 }
 interface AllDetailsType {
-  data:{[index:string]:string};
+  data: { [index: string]: string };
 }
 
-
 const postDetailsRequest = async (planetName: string) => {
-  const req:SearchType = await axios.post('http://localhost:8080/planet/search', {
-    name: planetName,
-  });
-  const key:string = req.data[0].key;
+  const req: SearchType = await axios.post(
+    'http://localhost:8080/planet/search',
+    {
+      name: planetName,
+    }
+  );
+  const key: string = req.data[0].key;
   return key;
 };
 
 const allDetails = async (planetName: string) => {
-  const end:string = await postDetailsRequest(planetName);
-  const res:AllDetailsType = await axios.get('http://localhost:8080/planet/' + end);
+  const end: string = await postDetailsRequest(planetName);
+  const res: AllDetailsType = await axios.get(
+    'http://localhost:8080/planet/' + end
+  );
   const b: { [index: string]: string } = res.data;
   return b;
 };
--- planet_game/src/API/getPlanetNames.ts
+++ planet_game/src/API/getPlanetNames.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import { ResponseTypes } from './ResponseType';
 
-
 const getNames = async () => {
-  const resp:ResponseTypes = await axios.get('http://localhost:8080/planet');
+  const resp: ResponseTypes = await axios.get('http://localhost:8080/planet');
   const a: string[] = resp.data;
   return a;
 };
